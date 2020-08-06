e65d1eaf8c0bff0ff5f82e4eed70f00231a9b6e8
Naveen Nagabhushan <naveen.nagabhushan@boeing.com>
2020-08-06 16:29:38 +0530

    Added method Comments

--- src/main/java/com/nvn/vendingmachine/service/VendingMachineService.java
+++ src/main/java/com/nvn/vendingmachine/service/VendingMachineService.java
@@ -29,6 +29,67 @@ public class VendingMachineService implements IVendingMachineService {
 		return vendingMachineService;
 	}
 
+	// Buying from vending machine
+	public Result buyProduct(String product, int quantity, List<Coins> coins) throws ProductNotFoundException,
+			InsufficientStockException, InsufficientMoneyInsertedException, InvalidQuantityException {
+
+		ProductStock productStock = repository.getProduct(product);
+
+		int moneyInserted = calculatedMoneyInserted(coins);
+
+		if (quantity <= 0) {
+			throw new InvalidQuantityException(ErrorMessage.INVALID_QUANTITY.getMessage());
+		}
+
+		// Product not present in Vending machine
+		if (productStock == null) {
+			throw new ProductNotFoundException(ErrorMessage.PRODUCT_NOT_FOUND.getMessage());
+		} else {
+			
+			// Requested quantity cannot be fulfilled because of low stock
+			if (productStock.getQuantity() < quantity) {
+				throw new InsufficientStockException(ErrorMessage.LOW_STOCK.getMessage());
+			}
+			
+			// Total money inserted is not sufficient to buy the product
+			if ((productStock.getProduct().getPrice() * quantity) > moneyInserted) {
+				throw new InsufficientMoneyInsertedException(ErrorMessage.INSUFFIENT_FUNDS_INSERT.getMessage());
+			}
+
+			repository.buyProduct(productStock.getProduct(), quantity);
+			return new Result(productStock.getProduct(),
+					renderChange(productStock.getProduct().getPrice() * quantity, moneyInserted));
+		}
+
+	}
+
+	// Add list of products
+	public boolean addProduct(List<ProductStock> products)
+			throws InvalidQuantityException, InvalidProductNameException, InvalidProductPriceException {
+
+		for (ProductStock prod : products) {
+			
+			// Check for null or blank product name
+			if (prod.getProduct().getName() == null || prod.getProduct().getName().isEmpty()) {
+				throw new InvalidProductNameException(ErrorMessage.ENTER_VALID_PRODUCT_NAME.getMessage());
+			}
+
+			// Check for 0 quantity
+			if (prod.getQuantity() == 0) {
+				throw new InvalidQuantityException(ErrorMessage.QUANTITY_CANT_BE_ZERO.getMessage());
+			}
+			
+			// Price of the product should not be zero
+			if(prod.getProduct().getPrice() <= 0) {
+				throw new InvalidProductPriceException(ErrorMessage.INVALID_PRODUCT_PRICE.getMessage());
+			}
+
+			addProduct(prod.getProduct(), prod.getQuantity());
+		}
+
+		return true;
+	}
+	
 	private void addProduct(Product product, int quantity) {
 
 		ProductStock stock = repository.getProduct(product);
@@ -43,61 +104,13 @@ public class VendingMachineService implements IVendingMachineService {
 
 	}
 
-	public Result buyProduct(String product, int quantity, List<Coins> coins) throws ProductNotFoundException,
-			InsufficientStockException, InsufficientMoneyInsertedException, InvalidQuantityException {
-
-		ProductStock productStock = repository.getProduct(product);
-
-		int moneyInserted = calculatedMoneyInserted(coins);
-
-		if (quantity <= 0) {
-			throw new InvalidQuantityException(ErrorMessage.INVALID_QUANTITY.getMessage());
-		}
-
-		if (productStock == null) {
-			throw new ProductNotFoundException(ErrorMessage.PRODUCT_NOT_FOUND.getMessage());
-		} else {
-			if (productStock.getQuantity() < quantity) {
-				throw new InsufficientStockException(ErrorMessage.LOW_STOCK.getMessage());
-			}
-			if ((productStock.getProduct().getPrice() * quantity) > moneyInserted) {
-				throw new InsufficientMoneyInsertedException(ErrorMessage.INSUFFIENT_FUNDS_INSERT.getMessage());
-			}
-
-			repository.buyProduct(productStock.getProduct(), quantity);
-			return new Result(productStock.getProduct(),
-					renderChange(productStock.getProduct().getPrice() * quantity, moneyInserted));
-		}
-
-	}
-
-	public boolean addProduct(List<ProductStock> products)
-			throws InvalidQuantityException, InvalidProductNameException, InvalidProductPriceException {
-
-		for (ProductStock prod : products) {
-			if (prod.getProduct().getName() == null || prod.getProduct().getName().isEmpty()) {
-				throw new InvalidProductNameException(ErrorMessage.ENTER_VALID_PRODUCT_NAME.getMessage());
-			}
-
-			if (prod.getQuantity() == 0) {
-				throw new InvalidQuantityException(ErrorMessage.QUANTITY_CANT_BE_ZERO.getMessage());
-			}
-			
-			if(prod.getProduct().getPrice() <= 0) {
-				throw new InvalidProductPriceException(ErrorMessage.INVALID_PRODUCT_PRICE.getMessage());
-			}
-
-			addProduct(prod.getProduct(), prod.getQuantity());
-		}
-
-		return true;
-	}
-
+	// Add and get total coint value
 	private int calculatedMoneyInserted(List<Coins> coins) {
 
 		return coins.stream().map(Coins::getCoinValue).reduce(0, Integer::sum);
 	}
 
+	// Return cahnge to the usee
 	private int renderChange(int productCost, int moneyInserted) {
 		return moneyInserted - productCost;
 	}
